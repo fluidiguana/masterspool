dee15e7007bacf1552418066f0eb4d34194a44b8
fluidiguana <135795572+fluidiguana@users.noreply.github.com>
2024-03-29 23:36:59 -0700

    Update teams.js to GCP v4

--- teams.js
+++ teams.js
@@ -1,29 +1,20 @@
 function fetchTeams() {
-  const documentId = '2PACX-1vTdEdeCu-lXdlVu0Z4VHHlpb094arTOFhCVpeANpxdA9PHf6LcAQ-awuBr72rey7hNMslAWiVoxXh4c';
-  fetch(`https://spreadsheets.google.com/feeds/cells/${documentId}/1/public/full?alt=json`)
+  const apiKey = '6cc00315d47e53a4e24aed427a82217ccb0c1a5c	'; // Replace this with your actual API key
+  const spreadsheetId = '13UdzO_PF24UcxXdVJGseIzhfsB9HkMO9mp8aT7c-pR4'; // Use your actual Spreadsheet ID
+  const range = 'Teams'; // Adjust based on your actual data range, e.g., 'Sheet1!A2:K'
+  const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${apiKey}`;
+
+  fetch(url)
     .then(response => response.json())
     .then(data => {
+      const rows = data.values; // 'values' contains an array of arrays, each representing a row of the sheet.
       teamsTableBody.innerHTML = ''; // Clear existing rows
 
-      // The actual data starts at the 8th entry in the feed.entry array
-      const teams = data.feed.entry.slice(1);
-
-      teams.forEach(team => {
-        const row = document.createElement('tr');
-        row.innerHTML = `
-          <td class ="bold-column">${team.Teams$team.$t}</td>
-          <td>${team.Teams$Player1.$t}</td>
-          <td>${team.Teams$Player2.$t}</td>
-          <td>${team.Teams$Player3.$t}</td>
-          <td>${team.Teams$Player4.$t}</td>
-          <td>${team.Teams$Player5.$t}</td>
-          <td>${team.Teams$Player6.$t}</td>
-          <td>${team.Teams$Player7.$t}</td>
-          <td>${team.Teams$Player8.$t}</td>
-          <td>${team.Teams$Player9.$t}</td>
-          <td>${team.Teams$Player10.$t}</td>
-        `;
-        teamsTableBody.appendChild(row);
+      // Assuming the first row in 'rows' contains column headers and actual data starts from the second row
+      rows.slice(1).forEach((row, index) => {
+        const teamRow = document.createElement('tr');
+        teamRow.innerHTML = row.map(cell => `<td>${cell}</td>`).join(''); // Create a cell for each column
+        teamsTableBody.appendChild(teamRow);
       });
     })
     .catch(error => console.error('Error:', error));
