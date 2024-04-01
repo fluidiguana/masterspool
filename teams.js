44ab49a85cfa26c9e3a69c170ffb46f060d6e9ec
fluidiguana <135795572+fluidiguana@users.noreply.github.com>
2024-03-31 19:02:36 -0700

    Update teams.js

--- teams.js
+++ teams.js
@@ -1,21 +1,31 @@
-function fetchTeams() {
-  const apiKey = '6cc00315d47e53a4e24aed427a82217ccb0c1a5c	'; // Replace this with your actual API key
-  const spreadsheetId = '13UdzO_PF24UcxXdVJGseIzhfsB9HkMO9mp8aT7c-pR4'; // Use your actual Spreadsheet ID
-  const range = 'Teams'; // Adjust based on your actual data range, e.g., 'Sheet1!A2:K'
-  const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${apiKey}`;
-
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      const rows = data.values; // 'values' contains an array of arrays, each representing a row of the sheet.
-      teamsTableBody.innerHTML = ''; // Clear existing rows
-
-      // Assuming the first row in 'rows' contains column headers and actual data starts from the second row
-      rows.slice(1).forEach((row, index) => {
-        const teamRow = document.createElement('tr');
-        teamRow.innerHTML = row.map(cell => `<td>${cell}</td>`).join(''); // Create a cell for each column
-        teamsTableBody.appendChild(teamRow);
-      });
-    })
-    .catch(error => console.error('Error:', error));
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const teamsTableBody = document.getElementById('teamsTableBody');
+  
+    function fetchTeams() {
+      fetch('http://localhost:3001/api/teams')
+        .then(response => response.json())
+        .then(data => {
+          teamsTableBody.innerHTML = ''; // Clear existing rows
+          data.forEach(team => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+              <td class ="bold-column">${team.TeamName}</td>
+              <td>${team.Player1}</td>
+              <td>${team.Player2}</td>
+              <td>${team.Player3}</td>
+              <td>${team.Player4}</td>
+              <td>${team.Player5}</td>
+              <td>${team.Player6}</td>
+              <td>${team.Player7}</td>
+              <td>${team.Player8}</td>
+              <td>${team.Player9}</td>
+              <td>${team.Player10}</td>
+            `;
+            teamsTableBody.appendChild(row);
+          });
+        })
+        .catch(error => console.error('Error:', error));
+    }
+    
+    fetchTeams(); // Fetch teams when the page loads
+  });
