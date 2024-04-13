24776e750e452d908402ad51ca69ec9cc9ff38a5
fluidiguana <135795572+fluidiguana@users.noreply.github.com>
2024-04-13 14:07:57 -0700

    Delete datatable-setup.js

--- datatable-setup.js
+++
@@ -1,7 +0,0 @@
-import DataTable from 'datatables.net-dt';
-
-document.addEventListener('DOMContentLoaded', function() {
-    let table = new DataTable('#leaderboard', {
-        // config options...
-    });
-});
